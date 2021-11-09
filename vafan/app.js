e417e64a38a09f86bff3eced5a02ee6bad31331c
ThuniZ <89513533+ThuniZ@users.noreply.github.com>
2021-11-09 16:12:05 +0100

    second commit

--- vafan/app.js
+++ vafan/app.js
@@ -7,30 +7,49 @@ const killers = [
     {killer: "michael_myers.jpg", name: "Michael Myers"}, {killer: "scream.jpg", name: "Scream"}
 ];
 
-//open the modal by clicking the img
-/*
-document.getElementById("mini-killers").innerHTML = killers
-.map((killers) => `<img src="./img/${killers.killer}">`).join("");
-*/
+//open and close the modal by clicking the img
+
 
 const closeKillerModal = () => {
     document.getElementById("killer-modal").style.display = "none";
+    document.getElementsByClassName("killer-image").style.border = "0px"
 }
+
+//Checks which img you clicked on and checks the array for that timg and opens it
+//Changes killer-modal to flex, and opens the img
+//the next loop checks which img you clicked on and opens that
+//and sets a border on that thumbnail and switches it when you select a new one
+//creates a span with a onclick with and X to close the modal
 
 
 const openKillerModal = () => {
-    //document.getElementById("killer-modal").style.display = "flex";
     const killer = document.getElementsByClassName("killer-image");
     for(let i = 0; i < killer.length; i++){
         killer[i].addEventListener("click", () => {
             document.getElementById("killer-modal").style.display = "flex";
             for(let k = 0; k < killers.length; k++){
                 if(i === k){
-                document.getElementById("killer-modal").innerHTML = `<img src="./img/${killers[k].killer}">`+ `<span onclick="closeKillerModal()"><i class="fa fa-times"></i></span>`;
-                
+                document.getElementById("killer-modal").innerHTML = `<span onclick="closeKillerModal()"><i class="fa fa-times"></i></span>` 
+                +  `<div id="main-killer"><img src="./img/${killers[k].killer}"></div>`
+                + `<div id="killer-thumbnail-modal" ></div> `;
+                killerThumbModal();
+                killer[k].style.border = "2px solid black";
+                } else {
+                    killer[k].style.border = "0px";
                 }
             }
         });
     }
   };
 
+
+//Map all the img in the array to a div under the main img
+const killerThumbModal = () => {
+    document.getElementById("killer-thumbnail-modal").innerHTML = killers.map((murder) => `<img src="./img/${murder.killer}" onclick="openKillerModal(this.src)">`).join("")
+    
+};
+
+const switchKiller = () => {
+
+    document.getElementById("main-killer").setAttribute("src", )
+};
